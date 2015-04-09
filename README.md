965730be878ceb0358c4daa0f3f8158580a763e3
Tawfek Daghistani <tawfekov@gmail.com>
2015-04-09 14:52:04 +0300

    add PHP docs , update readme.me

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# yellow-sdk-php
+# Yellow SDK PHP
+## Introduction :
+
+
+## Installation :
+Get more information  with the
+[Documentation](http://yellowpay.co/docs/api/).
+
+### Installing via Composer
+
+The recommended way to install Yellow PHP SDK is through
+[Composer](http://getcomposer.org).
+
+```bash
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+```
+
+Next, run the Composer command to install the latest master version of Yellow PHP SDK:
+
+```bash
+php composer.phar require yellow/php-sdk
+```
+
+After installing, you need to require Composer's autoloader:
+
+```php
+require 'vendor/autoload.php';
+```
+## Example :
+```php
+<?php
+use Yellow\Bitcoin\Invoice;
+$api_key = "Your API public key";
+$api_secert = "Your API Private key";
+$yellow = new Invoice($api_key,$api_secert);
+/// this will return an array with invoice data
+$invoice  = $yellow->createInvoice(10,"USD","http://yourstore.local/checkout/status/");
+var_dump($invoice);
+/// this will return an array with invoice status data
+$status = $yellow->checkInvoiceStatus($invoice["id"]);
+var_dump($status);
+```
+
+### Documentation
+
+More information can be found in the online documentation at
+http://yellowpay.co/docs/api/.
--- src/Bitcoin/Invoice.php
+++ src/Bitcoin/Invoice.php
@@ -2,13 +2,16 @@
 namespace Yellow\Bitcoin;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Exception\ClientException;
-
+use Exception ;
 
 class Invoice implements InvoiceInterface
 {
+    /**
+     * sdk version
+     */
     const VERSION = "0.1";
+
     /**
      * Server Root for Yellow API
      *
@@ -44,85 +47,122 @@ class Invoice implements InvoiceInterface
      */
     private $api_secret;
 
+    /**
+     * constructor method
+     * @note : if we want to use custom API server , we will read it automatically from EVN var
+     * @param $api_key => public key
+     * @param $api_secret => private key
+     */
     public function __construct($api_key, $api_secret)
     {
         /// set custom API server
-        $custom_server_root = getenv("YELLOW_API_SERVER");
+        $custom_server_root    = getenv("YELLOW_API_SERVER");
         if ($custom_server_root) {
             $this->server_root = $custom_server_root;
         }
-        $this->api_key = $api_key;
+        $this->api_key    = $api_key;
         $this->api_secret = $api_secret;
         return $this;
     }
 
+    /**
+     *
+     * @param $amount double
+     * @param $currency string
+     * @param string $callback string optional
+     * @return \GuzzleHttp\Message\ResponseInterface|mixed|null
+     */
     public function createInvoice($amount, $currency,$callback = "")
     {
         try {
-            $payload = [
+        	$payload = [
                 'base_ccy'   => $currency,
                 'base_price' => $amount,
                 'callback'   => $callback
             ];
-            $body = json_encode($payload);
-            $nonce = round(microtime(true) * 1000);
-            $url = $this->server_root . $this->api_uri_create_invoice;
-            $message = $nonce . $url . $body;
-            $signature = hash_hmac("sha256", $message, $this->api_secret, false);
-            $data = $this->createHTTPData($signature,$nonce);
-            $data["body"] =  $body;
-            $client = new Client();
+            $url      = $this->server_root . $this->api_uri_create_invoice;
+            $data     = $this->createHTTPData($url , $payload);
+            $client   = new Client();
             $response = $client->post($url, $data);
-            $body = $response->getBody();
-            return (string)$body;
+            $body     = (string) $response->getBody();
+            return json_decode($body , true);
         } catch (ClientException $e) {
             if ($e->hasResponse()) {
                 return $e->getResponse();
             }
+        } catch (Exception $e){
+            return $e->getTraceAsString();
         }
-
-
     }
 
+    /**
+     * check invoice status
+     * @param $id
+     * @return \GuzzleHttp\Message\ResponseInterface|mixed|null
+     */
     public function checkInvoiceStatus($id)
     {
         try {
-            $url = $this->server_root . str_replace("[id]", $id, $this->api_uri_check_payment);
-            $nonce = round(microtime(true) * 1000);
-            $message = $nonce . $url;
-            $signature = hash_hmac("sha256", $message, $this->api_secret, false);
-            $data = $this->createHTTPData($signature,$nonce);
-            $client = new Client();
+            $url      = $this->server_root . str_replace("[id]", $id, $this->api_uri_check_payment);
+            $data     = $this->createHTTPData($url , array() ,$id);
+            $client   = new Client();
             $response = $client->get($url, $data);
-            $body = $response->getBody();
-            return (string)$body;
+            $body     = (string) $response->getBody();
+            return json_decode($body , true);
         } catch (ClientException $e) {
             if ($e->hasResponse()) {
                 return $e->getResponse();
             }
+        } catch (Exception $e){
+            return $e->getTraceAsString();
         }
-
     }
 
-
+    /**
+     * get OS / PHP version
+     *
+     * @return string
+     */
     private function getOSVersion()
     {
         return PHP_OS . " - PHP " . PHP_VERSION;
     }
 
-    private function createHTTPData($signature,$nonce)
+    /**
+     * creates the http data array for both createInvoice / checkInvoiceStatus
+     *
+     * @param $url url used to create signature
+     * @param array $payload payload array
+     * @param string $id invoice id
+     * @return array
+     */
+    private function createHTTPData($url , $payload = array() , $id = "")
     {
-        return [
-            'headers' => [
-                'API-Key' => $this->api_key,
-                'API-Nonce' => $nonce,
-                'API-Sign' => $signature,
+    	$nonce = round(microtime(true) * 1000);
+	    if(!empty($payload)){
+        	$body        = json_encode($payload);
+        	$message     = $nonce . $url . $body;
+        	$append_body = true;
+        }else{
+        	$message     = $nonce . $url;
+        	$append_body = false;
+        }
+	    $signature = hash_hmac("sha256", $message, $this->api_secret, false);
+        $data = [
+            'headers'          => [
+                'API-Key'      => $this->api_key,
+                'API-Nonce'    => $nonce,
+                'API-Sign'     => $signature,
                 'API-Platform' => $this->getOSVersion(),
-                'API-Plugin' => self::VERSION,
+                'API-Plugin'   => self::VERSION,
                 'content-type' => 'application/json'
             ],
-            'allow_redirects' => false,
-            'timeout' => 300
+            'allow_redirects'  => false,
+            'timeout'          => 300
         ];
+        if($append_body){
+        	$data["body"] = $body;
+        }
+        return $data;
     }
 }
